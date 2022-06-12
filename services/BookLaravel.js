506a117b369c38cbd7cc6c21e68b1fff534b953d
Natthaphong15 <95035483+Natthaphong15@users.noreply.github.com>
2022-06-12 15:24:13 +0700

    week12

--- services/BookLaravel.js
+++ services/BookLaravel.js
@@ -1,5 +1,8 @@
+import * as mime from "mime";
+
 // const url_endpoint = "http://localhost/laravel8/public/api/book";
-const url_endpoint = "http://192.168.21.25/laravel8/public/api/book";
+//const url_endpoint = "http://192.168.21.25/laravel8/public/api/book";
+const url_endpoint = "http://192.168.130.93/laravel8/public/api/book";
 
 const getItems = async () => {
   try {
@@ -10,7 +13,6 @@ const getItems = async () => {
     console.log(error);
   }
 };
-
 const getItemDetail = async (item) => {
   try {
     let response = await fetch(url_endpoint + "/" + item.id);
@@ -23,11 +25,31 @@ const getItemDetail = async (item) => {
 
 const storeItem = async (item) => {
   try {
+    //CREATE FORM DATA
+    let data = new FormData();
+    for (let key in item) {
+      if (key == "image") continue;
+      data.append(key, item[key]);
+    }
+    if (item.image.split(":")[0] == "file") {
+      //ATTACHED FILE
+      data.append("image", {
+        uri: item.image,
+        name: item.image.split("/").pop(),
+        type: mime.getType(item.image),
+      });
+    }
+
     let body = {
       method: "POST",
       body: JSON.stringify(item),
       headers: { "Content-Type": "application/json" },
+      // body: JSON.stringify(item),
+      body: data,
+      // headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "multipart/form-data" },
     };
+
     let response = await fetch(url_endpoint, body);
     let result = await response.json();
     return result;
@@ -38,32 +60,54 @@ const storeItem = async (item) => {
 
 const updateItem = async (item) => {
   try {
+    //CREATE FORM DATA
+    console.log("UPDATE", item);
+    let data = new FormData();
+    for (let key in item) {
+      if (key == "image") continue;
+      data.append(key, item[key]);
+    }
+    data.append("_method", "PUT");
+    if (item.image.split(":")[0] == "file") {
+      //ATTACHED FILE
+      data.append("image", {
+        uri: item.image,
+        name: item.image.split("/").pop(),
+        type: mime.getType(item.image),
+      });
+    }
+    console.log(data);
     let body = {
       method: "PUT", //PUT or PATCH
       body: JSON.stringify(item),
       headers: { "Content-Type": "application/json" },
-    };
-    let response = await fetch(url_endpoint + "/" + item.id, body);
-    let result = await response.json();
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const destroyItem = async (item) => {
-  try {
-    let body = {
-      method: "DELETE",
+      method: "POST",
       // body: JSON.stringify(item),
-      headers: { "Content-Type": "application/json" },
+      body: data,
+      // headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "multipart/form-data" },
     };
+
     let response = await fetch(url_endpoint + "/" + item.id, body);
     let result = await response.json();
-    return result;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export default { getItems, getItemDetail , storeItem , updateItem , destroyItem};
+    return result; } catch (error) {
+      console.log(error);
+    }
+  };
+  const destroyItem = async (item) => {
+    try {
+      let body = {
+        method: "DELETE",
+        // body: JSON.stringify(item),
+        headers: { "Content-Type": "application/json" },
+      };
+      let response = await fetch(url_endpoint + "/" + item.id, body);
+      let result = await response.json();
+      return result;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  export default { getItems, getItemDetail , storeItem , updateItem , destroyItem};
+  
